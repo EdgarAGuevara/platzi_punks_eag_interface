e2d0ea2d6ef8e7d4c806fc86ea829f0f133718d9
Edgar guevara <edgargn@edgars-MacBook-Pro-2.local>
2022-08-22 11:28:09 -0500

    add all layouts resources that are images and layouts files and also connect to web3 hook to connect to metamask

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home/index.js";
-import Web3 from "web3/dist/web3.min";//-> in this way works with webpack > 5
+import MainLayout from "./layouts/main";
 
 function App() {
-    useEffect(() => {
-        if (window.ethereum) {
-            //Old implementation
-            // window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => { console.log(accounts) });
-
-            const web3 = new Web3(window.ethereum);
-            web3.eth.requestAccounts().then(console.log);
-
-        }
-
-    }, []);
 
     return (
-        //Ya que en la V6 de React Router se remplaza “component” por “element”, https://github.com/remix-run/react-router/blob/main/docs/upgrading/v5.md#advantages-of-route-element
-        < Routes >
-            <Route path="/" element={<Home />} />
-        </Routes >
+        <MainLayout>
+            < Routes >
+                <Route path="/" element={<Home />} />
+            </Routes >
+        </MainLayout>
+
     );
 }
 
