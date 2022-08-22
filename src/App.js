e5e5aa4974c8c77d433553b3a29b06c41808fe7d
Edgar guevara <edgargn@edgars-MacBook-Pro-2.local>
2022-08-22 10:46:12 -0500

    add initial resources to start to managment web3js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/home/index.js";
+import Web3 from "web3/dist/web3.min";//-> in this way works with webpack > 5
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        if (window.ethereum) {
+            //Old implementation
+            // window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => { console.log(accounts) });
+
+            const web3 = new Web3(window.ethereum);
+            web3.eth.requestAccounts().then(console.log);
+
+        }
+
+    }, []);
+
+    return (
+        //Ya que en la V6 de React Router se remplaza “component” por “element”, https://github.com/remix-run/react-router/blob/main/docs/upgrading/v5.md#advantages-of-route-element
+        < Routes >
+            <Route path="/" element={<Home />} />
+        </Routes >
+    );
 }
 
 export default App;
