be53633197c1ac12a90fe5fd87212090276cf6dd
Edgar A Guevara <edgarguevara33@gmail.com>
2022-08-25 10:09:46 -0500

    feat hidden keys of my project

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 
 cd ./build
 FILES=$(find * -type f | awk -v q="'" '{print " -F " q "file=@\"" $0 "\";filename=\"" $0 "\"" q}')
-curl "https://ipfs.infura.io:5001/api/v0/add?pin=true&recursive=true&wrap-with-directory=true&cid-version=1" -vv -X POST $FILES -u "2DqvKUXZ5UTUAxcUGBSmAKKIYHP:4231929c50cfdbce1ab0971ccf2b1204"
+curl "https://ipfs.infura.io:5001/api/v0/add?pin=true&recursive=true&wrap-with-directory=true&cid-version=1" -vv -X POST $FILES -u "PROJECT_ID:API_KEY_SECRET"
 cd ..
+
+#REmenber that PROJECT_ID:API_KEY_SECRET are on infura https://infura.io/dashboard/ipfs/2DqvKUXZ5UTUAxcUGBSmAKKIYHP/settings
